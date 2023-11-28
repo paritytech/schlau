c22c06bd6245844420c2febb381b62b35e5b87e6
Andrew Jones <ascjones@gmail.com>
2023-11-28 10:02:40 +0000

    WIP

---
+++ contracts/ink/crypto/lib.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+pub mod bench {
+    #[ink(storage)]
+    pub struct Bench {}
+
+    impl Bench {
+        #[allow(clippy::new_without_default)]
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message)]
+        pub fn sha3(&self, iterations: u32) {
+            use sha3::{
+                Digest,
+                Sha3_256,
+            };
+
+            for i in 0..iterations {
+                let mut hasher = Sha3_256::new();
+                hasher.update(i.to_le_bytes());
+                let _result = hasher.finalize();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+mod ink_build;
+
+use contract_build::Target;
+use drink::{
+    pallet_contracts,
+    runtime::MinimalRuntime,
+    Sandbox,
+    DEFAULT_GAS_LIMIT,
+};
+
+fn main() -> anyhow::Result<()> {
+    let contract = "contracts/ink/crypto/Cargo.toml";
+    let build_result = ink_build::build_contract(contract, Target::RiscV)?;
+
+    let mut sandbox = Sandbox::<MinimalRuntime>::new().expect("Failed to initialize Drink! sandbox");
+
+    let code = std::fs::read(build_result)?;
+    let value = 0;
+    let data = Vec::new(); // todo: use CreateBuilderPartial
+    let salt = || Vec::new();
+    let caller = [0u8; 32];
+    let gas_limit = DEFAULT_GAS_LIMIT;
+    let storage_deposit_limit = None;
+
+    let result = sandbox.deploy_contract(
+        code,
+        value,
+        data,
+        salt(),
+        caller.into(),
+        gas_limit,
+        storage_deposit_limit,
+    );
+
+    Ok(())
 }
+

d205d7d149a451b2c9076d69f5004e647e828eca
Andrew Jones <ascjones@gmail.com>
2023-12-01 15:04:55 +0000

    Works now

--- benches/crypto.rs
+++ benches/crypto.rs
@@ -8,38 +8,43 @@ use schlau::{
 };
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| {
-        let contract = "contracts/ink/crypto/Cargo.toml";
-        let build_result = ink_build::build_contract(contract, Target::RiscV).unwrap();
-        let code = std::fs::read(build_result).unwrap();
+    let contract = "contracts/ink/crypto/Cargo.toml";
+    let build_result = ink_build::build_contract(contract, Target::RiscV).unwrap();
+    let code = std::fs::read(build_result).unwrap();
 
-        let mut drink_api = DrinkApi::<DefaultEnvironment, MinimalRuntime>::new();
+    let mut drink_api = DrinkApi::<DefaultEnvironment, MinimalRuntime>::new();
 
-        let value = 0;
-        let salt = Vec::new();
-        let storage_deposit_limit = None;
+    let value = 0;
+    let salt = Vec::new();
+    let storage_deposit_limit = None;
 
-        use crypto::crypto::{Crypto, CryptoRef};
+    use crypto::crypto::{Crypto, CryptoRef};
 
-        let mut constructor = CryptoRef::new();
+    let mut constructor = CryptoRef::new();
 
-        let contract = drink_api
-            .ink_instantiate_with_code::<Crypto, _, _>(
-                code,
-                value,
-                &mut constructor,
-                salt,
-                &subxt_signer::sr25519::dev::alice(),
-                storage_deposit_limit,
-            )
-            .unwrap();
+    let contract = drink_api
+        .ink_instantiate_with_code::<Crypto, _, _>(
+            code,
+            value,
+            &mut constructor,
+            salt,
+            &subxt_signer::sr25519::dev::alice(),
+            storage_deposit_limit,
+        )
+        .unwrap();
 
-        let message = contract.sha3(1000);
+    let message = contract.sha3(10);
 
-        let call_args = CallArgs::from_call_builder(&subxt_signer::sr25519::dev::alice(), &message);
+    let call_args = CallArgs::from_call_builder(&subxt_signer::sr25519::dev::alice(), &message);
 
+    let mut group = c.benchmark_group("sample-size-example");
+    group.sample_size(10);
+
+    group.bench_function("sha3", |b| {
         b.iter(|| drink_api.call(call_args.clone()).unwrap())
     });
+
+    group.finish()
 }
 
 criterion_group!(benches, criterion_benchmark);

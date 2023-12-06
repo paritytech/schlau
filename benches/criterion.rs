35f189b57b44c9280d81fa6d1aec162d3a6520c2
Andrew Jones <ascjones@gmail.com>
2023-12-06 14:46:58 +0000

    Macro for ink contracts

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,50 +1,42 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use ink::env::DefaultEnvironment;
+use subxt_signer::sr25519::dev;
 
 use schlau::drink::runtime::MinimalRuntime;
 use schlau::drink_api::{CallArgs, DrinkApi};
 
-fn crypto(c: &mut Criterion) {
-    use crypto::crypto::{Crypto, CryptoRef};
+macro_rules! ink_contract_bench {
+    ( $name:ident, $contract:ident, $contract_ref:ident, $message:ident, $args:tt) => {
+        fn $name(c: &mut Criterion) {
+            use $name::$name::{$contract, $contract_ref};
 
-    let mut drink_api = DrinkApi::<DefaultEnvironment, MinimalRuntime>::new();
-    let contract = drink_api.build_and_instantiate::<_, Crypto, _, _>(
-        "contracts/ink/crypto/Cargo.toml",
-        &mut CryptoRef::new(),
-    );
+            let contract_name = stringify!($name);
 
-    let message = contract.sha3(100);
-    let call_args = CallArgs::from_call_builder(&subxt_signer::sr25519::dev::alice(), &message);
+            let mut drink_api = DrinkApi::<DefaultEnvironment, MinimalRuntime>::new();
+            let contract = drink_api.build_and_instantiate::<_, $contract, _, _>(
+                &format!("contracts/ink/{}/Cargo.toml", contract_name),
+                &mut $contract_ref::new(),
+            );
 
-    let mut group = c.benchmark_group("crypto");
-    group.sample_size(50);
+            let message = contract.$message($args);
+            let call_args = CallArgs::from_call_builder(&dev::alice(), &message);
 
-    group.bench_function("sha3", |b| {
-        b.iter(|| drink_api.call(call_args.clone()).unwrap())
-    });
-    group.finish()
+            let mut group = c.benchmark_group(contract_name);
+            group.sample_size(30);
+
+            let bench_name = format!("{}_{}", stringify!($message), stringify!($args));
+
+            group.bench_function(bench_name, |b| {
+                b.iter(|| drink_api.call(call_args.clone()).unwrap())
+            });
+
+            group.finish()
+        }
+    }
 }
 
-fn computation(c: &mut Criterion) {
-    use computation::computation::{Computation, ComputationRef};
-
-    let mut drink_api = DrinkApi::<DefaultEnvironment, MinimalRuntime>::new();
-    let contract = drink_api.build_and_instantiate::<_, Computation, _, _>(
-        "contracts/ink/computation/Cargo.toml",
-        &mut ComputationRef::new(),
-    );
-
-    let message = contract.odd_product(100_000);
-    let call_args = CallArgs::from_call_builder(&subxt_signer::sr25519::dev::alice(), &message);
-
-    let mut group = c.benchmark_group("computation");
-    group.sample_size(50);
-
-    group.bench_function("odd_product", |b| {
-        b.iter(|| drink_api.call(call_args.clone()).unwrap())
-    });
-    group.finish()
-}
+ink_contract_bench!(crypto, Crypto, CryptoRef, sha3, 100);
+ink_contract_bench!(computation, Computation, ComputationRef, odd_product, 100_000);
 
 criterion_group!(benches, crypto, computation);
 criterion_main!(benches);

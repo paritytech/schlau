9cba12bdbb7f7c7c0d2ee0b57ceb5ba7d463fde6
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-12-11 14:59:38 +0100

    add some solidity
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

---
+++ contracts/solidity/computation.sol
@@ -0,0 +1,14 @@
+contract Computation {
+    function triangle_number(int64 n) public pure {
+        int64 sum = 0;
+        for (int64 x = 1; x <= n; x++) {
+            sum += x;
+        }
+    }
+
+    function odd_product(int32 n) public pure returns (int64 prod) {
+        for (int32 x = 1; x <= n; x++) {
+            prod *= 2 * int64(x) - 1;
+        }
+    }
+}

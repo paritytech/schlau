83433bd49e45a4d9f03963d03a9930785bf1ed82
Andrew Jones <ascjones@gmail.com>
2023-12-13 15:29:27 +0000

    Try to get contract to build with solc

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,4 @@ pub mod drink_api;
 pub mod evm;
 pub mod ink;
 pub mod solang;
-mod solc;
+pub mod solc;
--- src/solc.rs
+++ src/solc.rs
@@ -8,19 +8,17 @@ use contract_build::Target;
 /// Builds the Solidity source in `path_to_source_sol`.
 /// Returns the path to the build output directory.
 ///
-/// For each each contract found in the source code, solang creates two files:
-/// - `contract_name.wasm`: The code blob.
-/// - `contract_name.contract`: The full contract artifact including metadata.
+/// For each each contract found in the source code, solc creates a file:
+/// - `contract_name.bin`: The code blob.
 /// Where `contract_name` is equal to the name of that contract
 ///
-/// Note: For RiscV the produced contract blob does still have the `.wasm` file extension.
 pub fn build_contract<P>(path_to_source_sol: P) -> anyhow::Result<PathBuf>
     where
         P: AsRef<Path> + Copy,
 {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let bin_path = PathBuf::from(manifest_dir).join("bin").join("solang");
+    let bin_path = PathBuf::from(manifest_dir).join("bin").join("solc");
 
     let out_dir = path_to_source_sol
         .as_ref()
@@ -28,13 +26,7 @@ pub fn build_contract<P>(path_to_source_sol: P) -> anyhow::Result<PathBuf>
         .expect("source file is not in root dir");
 
     match Command::new(&bin_path)
-        .arg("compile")
-        .arg("--target")
-        .arg("-O")
-        .arg("aggressive")
-        .arg("--release")
-        .arg("--wasm-opt")
-        .arg("z")
+        .arg("--bin")
         .arg("-o")
         .arg(out_dir)
         .arg(path_to_source_sol.as_ref())

2f209f64cc8ab524d77cb1c72cde7dbaed4ad9a0
Andrew Jones <ascjones@gmail.com>
2023-12-18 18:16:08 +0000

    evm load abi from output

--- benches/solidity.rs
+++ benches/solidity.rs
@@ -7,16 +7,12 @@ use schlau::evm::{CallArgs, CreateArgs, EvmRuntime, EvmSandbox, DEFAULT_ACCOUNT}
 use sp_core::U256;
 
 fn computation_evm(c: &mut Criterion) {
-    let contract = schlau::solc::build_contract("contracts/solidity/computation.sol").unwrap();
+    let result = schlau::solc::build_contract("contracts/solidity/computation.sol").unwrap();
     let mut sandbox = EvmSandbox::<EvmRuntime>::new();
-
-    let abi_path = "contracts/solidity/Computation.abi";
-    let json = std::fs::read_to_string(abi_path).unwrap();
-    let abi: JsonAbi = serde_json::from_str(&json).unwrap();
 
     let create_args = CreateArgs {
         source: DEFAULT_ACCOUNT,
-        init: contract,
+        init: result.code,
         gas_limit: 1_000_000_000,
         max_fee_per_gas: U256::from(1_000_000_000),
         ..Default::default()
@@ -29,7 +25,7 @@ fn computation_evm(c: &mut Criterion) {
     let n = 100_000;
     let bench_name = format!("odd_product_{}", n);
 
-    let func = &abi.function("odd_product").unwrap()[0];
+    let func = &result.abi.function("odd_product").unwrap()[0];
     let input = [DynSolValue::Int(I256::try_from(n).unwrap(), 32)];
     let data = func.abi_encode_input(&input).unwrap();
 
@@ -51,7 +47,7 @@ fn computation_evm(c: &mut Criterion) {
     let n = 100_000;
     let bench_name = format!("triangle_number_{}", n);
 
-    let func = &abi.function("triangle_number").unwrap()[0];
+    let func = &result.abi.function("triangle_number").unwrap()[0];
     let input = [DynSolValue::Int(I256::try_from(n).unwrap(), 64)];
     let data = func.abi_encode_input(&input).unwrap();
 

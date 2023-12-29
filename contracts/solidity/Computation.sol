493d74508afd8eb7ea9581915f83f21af73eb537
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-12-29 20:15:35 +0100

    wrapping_ math equals unchecked in Solidity
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- contracts/solidity/Computation.sol
+++ contracts/solidity/Computation.sol
@@ -1,13 +1,19 @@
 contract Computation {
     function triangle_number(int64 n) public pure returns (int64 sum) {
-        for (int64 x = 1; x <= n; x++) {
-            sum += x;
+        unchecked {
+            for (int64 x = 1; x <= n; x++) {
+                sum += x;
+            }
         }
     }
 
-    function odd_product(int32 n) public pure returns (int64 prod) {
-        for (int32 x = 1; x <= n; x++) {
-            prod *= 2 * int64(x) - 1;
+    function odd_product(int32 n) public pure returns (int64) {
+        unchecked {
+            int64 prod = 1;
+            for (int32 x = 1; x <= n; x++) {
+                prod *= 2 * int64(x) - 1;
+            }
+            return prod;
         }
     }
 }
